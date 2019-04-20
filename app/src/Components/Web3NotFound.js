bbf79be2bfe1d54ae676ebc18b62dd07437e6ec8
kombos <18464450+kombos@users.noreply.github.com>
2019-04-20 13:23:40 +0530

    updated css of web3NotFound page

--- app/src/Components/Web3NotFound.js
+++ app/src/Components/Web3NotFound.js
@@ -22,10 +22,17 @@ const styles = theme => ({
     flex: {
         flex: 1,
     },
+    dialogRoot: {
+        backgroundColor: "rgba(102,0,51,1)",
+        //color: "#17d4fe",
+        //height: '100%',
+        //width: 'auto',
+    },
     imageContainer: {
         boxSizing: 'border-box',
         //display: 'flex',
-        backgroundColor: 'rgba(51,51,51,1)',
+        //backgroundColor: 'rgba(51,51,51,1)',
+        backgroundColor: 'rgba(0,0,0,1)',
         width: '100%',
         textAlign: 'center',
     },
@@ -65,7 +72,18 @@ class Web3NotFound extends React.Component {
 
         return (
             <div>
-                <Dialog fullScreen disableBackdropClick disableEscapeKeyDown fullWidth open={isDialogOpen} onClose={this.handleClose} TransitionComponent={Transition}>
+                <Dialog
+                    fullScreen
+                    disableBackdropClick
+                    disableEscapeKeyDown
+                    fullWidth
+                    open={isDialogOpen}
+                    onClose={this.handleClose}
+                    TransitionComponent={Transition}
+                    classes={{
+                        paper: classes.dialogRoot,
+                    }}
+                >
                     <AppBar className={classes.appBar}>
                         <Toolbar>
                             <Typography variant="h6" color="inherit" className={classes.flex}>
@@ -82,7 +100,7 @@ class Web3NotFound extends React.Component {
                     </div>
                     <br />
                     <DialogContent>
-                        <DialogContentText>
+                        <DialogContentText color="error">
                             Multiprizer - Multiply your prize winnings by playing your tokens strategically!
                             <br />
                             This DApp requires MetaMask if you are in a Desktop / Laptop, or Trust Wallet App if you are in a Mobile device. Please select an item below.
@@ -91,13 +109,31 @@ class Web3NotFound extends React.Component {
                     <List>
                         <a href="https://metamask.io/">
                             <ListItem button >
-                                <ListItemText primary="Get Metamask" secondary="applicable for Desktops/Laptops" />
+                                <ListItemText
+                                    primary="Get Metamask"
+                                    secondary="applicable for Desktops/Laptops"
+                                    primaryTypographyProps={{
+                                        color: "primary"
+                                    }}
+                                    secondaryTypographyProps={{
+                                        color: "error"
+                                    }}
+                                />
                             </ListItem>
                         </a>
                         <Divider />
                         <a href="https://trustwallet.com/">
                             <ListItem button>
-                                <ListItemText primary="Get Trust Wallet" secondary="applicable for Mobile Devices" />
+                                <ListItemText
+                                    primary="Get Trust Wallet"
+                                    secondary="applicable for Mobile Devices"
+                                    primaryTypographyProps={{
+                                        color: "primary"
+                                    }}
+                                    secondaryTypographyProps={{
+                                        color: "error"
+                                    }}
+                                />
                             </ListItem>
                         </a>
                     </List>
